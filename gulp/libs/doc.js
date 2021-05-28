f699b1be5973ed1f2cb1dbfefb387004668f2d28
owenSai <33215096+owenSai@users.noreply.github.com>
2021-05-28 12:10:16 +0800

    Update doc.js

--- gulp/libs/doc.js
+++ gulp/libs/doc.js
@@ -6,6 +6,18 @@
 
 import gulp from 'gulp';
 
+gulp.task('test', gulp.series('doc:dev', 'doc:prod', 'doc:online', function() {
+  gulp.src('./src/common/doc/*')
+    .pipe(gulp.dest('./build/doc'));
+  gulp
+    .src('./build/doc/*')
+    .pipe(gulp.dest('./public/doc'));
+  return gulp
+    .src('./build/doc/*')
+    .pipe(gulp.dest('./online/doc'));
+  
+}));
+/*
 gulp.task('doc:dev', function () {
   return gulp.src('./src/common/doc/*')
     .pipe(gulp.dest('./build/doc'))
@@ -21,3 +33,4 @@ gulp.task('doc:online', ['doc:dev'], function () {
     .src('./build/doc/*')
     .pipe(gulp.dest('./online/doc'));
 });
+*/
